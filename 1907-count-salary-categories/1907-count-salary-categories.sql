15bd7c2d186661f8e13b01d20aa5f1956bfa9426
Varun N <114867149+itzVarun1426@users.noreply.github.com>
2026-08-11 20:35:41 +0530

    Time: 1803 ms (17.04%), Space: 0B (100%) - LeetHub

--- 1907-count-salary-categories/1907-count-salary-categories.sql
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -1,26 +1,45 @@
 # Write your MySQL query statement below
-SELECT categories.category , COALESCE(counts.accounts_count , 0) AS accounts_count
-FROM (
-    SELECT 'Low Salary' AS category 
-    UNION ALL 
-    SELECT 'Average Salary' 
-    UNION ALL 
-    SELECT 'High Salary'
-) categories
-LEFT JOIN (
-    #below table is used to have the count of each categorical income so that it can be joint to the categoies table to get the categoies those having 0 count and doesnt exist in below table
-    SELECT category , COUNT(income) AS accounts_count
-    FROM (
-        #to create a table with new column category and classifying each income into those categories but creating new table is done in select statement so group by cant be done in same query so ues it as subquery
-        SELECT 
-        *,
-        CASE 
-            WHEN income < 20000 THEN 'Low Salary'
-            WHEN income >= 20000 AND income <= 50000 THEN 'Average Salary'
-            ELSE 'High Salary'
-        END AS category
-        FROM Accounts
-    ) temp
-    GROUP BY category
-) counts
-ON categories.category = counts.category;
+-- SELECT categories.category , COALESCE(counts.accounts_count , 0) AS accounts_count
+-- FROM (
+--     SELECT 'Low Salary' AS category 
+--     UNION ALL 
+--     SELECT 'Average Salary' 
+--     UNION ALL 
+--     SELECT 'High Salary'
+-- ) categories
+-- LEFT JOIN (
+--     #below table is used to have the count of each categorical income so that it can be joint to the categoies table to get the categoies those having 0 count and doesnt exist in below table
+--     SELECT category , COUNT(income) AS accounts_count
+--     FROM (
+--         #to create a table with new column category and classifying each income into those categories but creating new table is done in select statement so group by cant be done in same query so ues it as subquery
+--         SELECT 
+--         *,
+--         CASE 
+--             WHEN income < 20000 THEN 'Low Salary'
+--             WHEN income >= 20000 AND income <= 50000 THEN 'Average Salary'
+--             ELSE 'High Salary'
+--         END AS category
+--         FROM Accounts
+--     ) temp
+--     GROUP BY category
+-- ) counts
+-- ON categories.category = counts.category;
+
+
+#this is not the only solution you can find below is also a solution for this questoin where 3 individual tables are created with each category and there count of the condition and union of all of them
+
+SELECT 'Low Salary' AS category,
+    COUNT(CASE WHEN income<20000 THEN 1 END) AS accounts_count
+FROM Accounts
+
+UNION ALL
+
+SELECT 'Average Salary' AS category,
+    COUNT(CASE WHEN income>=20000 AND income<=50000 THEN 1 END) AS accounts_count
+FROM Accounts
+
+UNION ALL
+
+SELECT 'High Salary' AS category,
+    COUNT(CASE WHEN income>50000 THEN 1 END) AS accounts_count
+FROM Accounts
