4f30f52eeb0fbb1fef5113e83107b66f305d4a61
Varun N <114867149+itzVarun1426@users.noreply.github.com>
2026-08-13 00:55:22 +0530

    Time: 0 ms (100%), Space: 23 MB (47.73%) - LeetHub

--- 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     int missingInteger(vector<int>& nums) {
-        unordered_map<int,bool>store;
-        int prefSum = nums[0];
-        bool isPref = true;
-        int temp = nums[0];
-        store[nums[0]] = true;
-        int n = nums.size();
-        for(int i = 1; i<n; i++){
+        map<int, bool> store;
+
+        for (int i = 0; i < nums.size(); i++) {
             store[nums[i]] = true;
-            if(isPref){
-                if(nums[i] == nums[i-1]+1)
-                {
-                    temp+=nums[i];
-                    prefSum = max(prefSum,temp);
-                }
-                else {
-                    temp = nums[i];
-                    isPref= false;
-                }
-            }
         }
-        for( ;  ;prefSum++)if(!store[prefSum])return prefSum;
-        return 0;
+
+        int prefixSum = nums[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i - 1] + 1)
+                prefixSum += nums[i];
+            else
+                break;
+        }
+
+        while (store[prefixSum])
+            prefixSum++;
+
+        return prefixSum;
     }
 };
