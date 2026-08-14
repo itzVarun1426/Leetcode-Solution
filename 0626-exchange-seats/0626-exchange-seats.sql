2af21858dbea15034ee31b81fda17db1dffe0912
Varun N <114867149+itzVarun1426@users.noreply.github.com>
2026-08-15 00:36:30 +0530

    Time: 438 ms (17.43%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,19 +1,33 @@
-# Write your MySQL query statement below
-SELECT 
+-- # Write your MySQL query statement below
+-- SELECT 
+--     id,
+--     CASE
+--         WHEN id = (SELECT MAX(id) FROM Seat) AND id % 2 = 1
+--         THEN student
+--         WHEN id % 2 = 1 
+--             THEN (
+--                 SELECT student 
+--                 FROM Seat 
+--                 WHERE id = s.id+1
+--             )
+--         ELSE (
+--             SELECT student 
+--             FROM Seat 
+--             WHERE id = s.id-1
+--         )
+--     END AS student
+-- FROM Seat s 
+
+-- above solution works its time heavy so below solution which is better in this way
+
+SELECT
     id,
     CASE
-        WHEN id = (SELECT MAX(id) FROM Seat) AND id % 2 = 1
-        THEN student
-        WHEN id % 2 = 1 
-            THEN (
-                SELECT student 
-                FROM Seat 
-                WHERE id = s.id+1
-            )
-        ELSE (
-            SELECT student 
-            FROM Seat 
-            WHERE id = s.id-1
-        )
+        WHEN id % 2 = 1 AND id != MAX(id) OVER ()
+            THEN LEAD(student) OVER (ORDER BY id)
+        WHEN id % 2 = 0
+            THEN LAG(student) OVER (ORDER BY id)
+        ELSE student
     END AS student
-FROM Seat s 
+FROM Seat
+ORDER BY id;
